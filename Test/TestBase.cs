ef6c77260df0312357785214dccefad23b9f4618
Sadorov Timur <sadorov2001@mail.ru>
2022-12-11 15:40:11 +0300

    fix

--- Test/TestBase.cs
+++ Test/TestBase.cs
@@ -6,50 +6,45 @@ namespace Test;
 
 public class TestBase
 {
-    private IWebDriver driver;
-    public IDictionary<string, object> vars { get; private set; }
-    private IJavaScriptExecutor js;
+    private IWebDriver _driver;
 
     [SetUp]
     public void SetUp()
     {
-        
-        driver = new ChromeDriver();
-        js = (IJavaScriptExecutor) driver;
-        vars = new Dictionary<string, object>();
+        _driver = new ChromeDriver();
     }
 
     [TearDown]
     protected void TearDown()
     {
-        driver.Quit();
+        _driver.Quit();
     }
     
     public void CreateList(ListData listData)
     {
-        driver.FindElement(By.Id("edit-project-name")).Click();
-        driver.FindElement(By.Id("edit-project-name")).SendKeys(listData.Name);
-        driver.FindElement(By.CssSelector(".ap-button-primary")).Click();
+        _driver.FindElement(By.Id("edit-project-name")).Click();
+        _driver.FindElement(By.Id("edit-project-name")).SendKeys(listData.Name);
+        _driver.FindElement(By.CssSelector(".ap-button-primary")).Click();
         Thread.Sleep(2000);
     }
 
     public void OpenCreateListForm()
     {
-        driver.FindElement(By.CssSelector(".hoverSection:nth-child(1) .add-icon use")).Click();
+        _driver.FindElement(By.CssSelector(".hoverSection:nth-child(1) .add-icon use")).Click();
     }
 
     public void Login(AccountData account)
     {
-        driver.FindElement(By.LinkText("Войти")).Click();
-        driver.FindElement(By.Id("emailOrPhone")).SendKeys(account.Email);
-        driver.FindElement(By.Id("password")).Click();
-        driver.FindElement(By.Id("password")).SendKeys(account.Password);
-        driver.FindElement(By.CssSelector(".button__3eXSs")).Click();
+        _driver.FindElement(By.LinkText("Войти")).Click();
+        _driver.FindElement(By.Id("emailOrPhone")).SendKeys(account.Email);
+        _driver.FindElement(By.Id("password")).Click();
+        _driver.FindElement(By.Id("password")).SendKeys(account.Password);
+        _driver.FindElement(By.CssSelector(".button__3eXSs")).Click();
         Thread.Sleep(7000);
     }
 
     public void OpenHomePage()
     {
-        driver.Navigate().GoToUrl("https://ticktick.com/");
+        _driver.Navigate().GoToUrl("https://ticktick.com/");
     }
 }
